0495fbe950c4b3e33617ee9e338b8aa0dfd2202c
Stuart Sierra <mail@stuartsierra.com>
2010-02-27 19:24:36 -0500

    Simplify time trial macros.
    Collecting time measurements in the code throws
    off the results; only the time macro is accurate.

--- time-trials.clj
+++ time-trials.clj
@@ -1,60 +1,49 @@
-(defmacro nanotime [reps body]
-  `(let [start# (System/nanoTime)]
-     (dotimes [i# ~reps] ~body)
-     (/ (- (System/nanoTime) start#) 1000000.0)))
-
-(defmacro time-trials [sets reps body]
-  `(for [i# (range ~sets)] (nanotime ~reps ~body)))
+(defmacro timetest
+  "Executes reps repetitions of body, five times, in the
+  clojure.core/time macro."
+  [reps body]
+  `(dotimes [j# 5] (time (dotimes [i# ~reps] ~body))))
 
 (defn clojure-version-string []
   (str (:major *clojure-version*) \.
        (:minor *clojure-version*) \.
        (:incremental *clojure-version*)
        (when-let [q (:qualifier *clojure-version*)]
-         (when-not (= q "") (str \- q)))))
+         (when-not (empty? q) (str \- q)))))
 
 (defmacro compare-times
-  "Measures time for n repetitions of each body, averaged over k runs
-  (omits first 2 runs from average).  Prints description, followed by
-  averages from body1 and body2."
-  [description k n body1 body2]
-  `(let [times1# (drop 2 (time-trials ~k ~n ~body1))
-         times2# (drop 2 (time-trials ~k ~n ~body2))]
-     (println (format "%30s %9.2f %9.2f" ~description
-                      (/ (apply + times1#) ~(- k 2))
-                      (/ (apply + times2#) ~(- k 2))))))
+  "Executes each body n times, prints description followed by elapsed
+  times."
+  [description n body1 body2]
+  `(do (println "\n###" (clojure-version-string)
+                "BEFORE" ~description)
+       (timetest ~n ~body1)
+       (println "###" (clojure-version-string)
+                "AFTER" ~description)
+       (timetest ~n ~body2)))
 
-(println (format "%-30s %9s %9s" (str "# Clojure " (clojure-version-string))
-                 "BEFORE" "AFTER"))
+(println "\n\n##### Clojure" (clojure-version-string))
 
 (let [s (identity "Hello, World!")]
-  (compare-times "Eliminate Reflection" 5 10000
+  (compare-times "Eliminating reflection" 10000
                  (.length s)
                  (.length #^String s)))
 
-(compare-times "Use primitives in loops"
-               5 100000
+(compare-times "Using primitive ops in loops" 100000
                (loop [sum 0, x 1]
                  (if (= x 100)
                    sum
                    (recur (+ sum x) (inc x))))
                (loop [sum (int 0), x (int 1)]
-                 (if (= x 100)
+                 (if (== x 100)
                    sum
                    (recur (unchecked-add sum x) (unchecked-inc x)))))
                
-
-(compare-times "Replace = with =="
-               5 1000000
-               (= 2 4) (== 2 4))
-
-(compare-times "Use Binary Arithmetic"
-               5 1000000
+(compare-times "Using binary arithmetic ops" 1000000
                (+ 2 4 6 8) (+ 2 (+ 4 (+ 6 8))))
 
 (let [v [1 2 3]]
-  (compare-times "Avoid Destructuring"
-                 5 1000000
+  (compare-times "Avoiding destructuring" 1000000
                  (let [[x y z] v] (+ x y z))
                  (let [x (nth v 0)
                        y (nth v 1)
@@ -63,7 +52,6 @@
 (def *value* 42)
 
 (let [value *value*]
-  (compare-times "Avoid Var lookups"
-                 5 1000000
+  (compare-times "Avoiding Var lookups" 1000000
                  (* *value* *value*)
                  (* value value)))
